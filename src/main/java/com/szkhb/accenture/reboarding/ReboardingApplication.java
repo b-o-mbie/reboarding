256d8392b8454221870be510cfa6d79f5e7128ae
Benedek Halasi <b@Benedeks-iMac.local>
2020-06-11 22:55:51 +0200

    office config added

--- src/main/java/com/szkhb/accenture/reboarding/ReboardingApplication.java
+++ src/main/java/com/szkhb/accenture/reboarding/ReboardingApplication.java
@@ -2,8 +2,10 @@ package com.szkhb.accenture.reboarding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class ReboardingApplication {
 
 	public static void main(String[] args) {
