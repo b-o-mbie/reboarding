39cc4aafc8c23413d3b037cf74e1afeae9f56029
Benedek Halasi <b@Benedeks-iMac.local>
2020-06-11 20:56:33 +0200

    pom.xml added log4j

---
+++ src/main/java/com/szkhb/accenture/reboarding/ReboardingApplication.java
@@ -0,0 +1,13 @@
+package com.szkhb.accenture.reboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReboardingApplication.class, args);
+	}
+
+}
