6bb696512ad26cb6aa9a3365bc719569534d7d00
Benedek Halasi <b@Benedeks-iMac.local>
2020-06-11 22:52:51 +0200

    OfficeConfig file added to spring configuration properties

---
+++ src/main/java/com/szkhb/accenture/reboarding/config/OfficeConfig.java
@@ -0,0 +1,16 @@
+package com.szkhb.accenture.reboarding.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "office")
+public class OfficeConfig {
+
+	private int capacity;
+	private int utilization;
+
+}
