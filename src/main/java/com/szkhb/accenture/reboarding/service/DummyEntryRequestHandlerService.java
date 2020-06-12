b3b927ee29761dfe22cbe919592198931a0ee6d4
Benedek Halasi <b@Benedeks-iMac.local>
2020-06-12 09:49:36 +0200

    entryrequesthandler interface & dummy service

---
+++ src/main/java/com/szkhb/accenture/reboarding/service/DummyEntryRequestHandlerService.java
@@ -0,0 +1,29 @@
+package com.szkhb.accenture.reboarding.service;
+
+public class DummyEntryRequestHandlerService implements EntryRequestHandlerService {
+
+	@Override
+	public String createNewRequest() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getStatusForRequest(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String tryEnter(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String tryExit(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
