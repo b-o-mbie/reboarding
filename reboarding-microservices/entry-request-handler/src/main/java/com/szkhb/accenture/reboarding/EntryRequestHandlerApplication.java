3df97a512e8cc387784519c9c0f608e5718d3827
Benedek Halasi <b@Benedeks-iMac.local>
2020-06-24 16:56:01 +0200

    gitlab ci config & maven-project cleanup

---
+++ reboarding-microservices/entry-request-handler/src/main/java/com/szkhb/accenture/reboarding/EntryRequestHandlerApplication.java
@@ -0,0 +1,50 @@
+package com.szkhb.accenture.reboarding;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.szkhb.accenture.reboarding.config.OfficeConfig;
+import com.szkhb.accenture.reboarding.domain.EntryRequest;
+import com.szkhb.accenture.reboarding.repository.EntryRequestRepository;
+
+@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
+public class EntryRequestHandlerApplication {
+
+	@Autowired
+	private OfficeConfig officeConfig;
+
+	@Autowired
+	private EntryRequestRepository entryRepÏo;
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntryRequestHandlerApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("CommandLineRunner has started.");
+			System.out.println(officeConfig);
+
+			System.out.println(new EntryRequest());
+			System.out.println(new EntryRequest());
+
+		};
+	}
+
+	@RequestMapping("/heartbeat")
+	public String heartbeat() {
+		return "Hello Docker World, this is " + this.getClass().getName();
+	}
+
+}
---
+++ reboarding-microservices/exit-service/src/main/java/com/szkhb/accenture/reboarding/ExitServiceApplication.java
@@ -0,0 +1,36 @@
+package com.szkhb.accenture.reboarding;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
+public class ExitServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExitServiceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("CommandLineRunner has started by " + this.getClass().getSimpleName());
+
+		};
+	}
+
+	@RequestMapping("/exit")
+	public String exit() {
+		return "Hello Docker World, this is " + this.getClass().getName();
+	}
+}
